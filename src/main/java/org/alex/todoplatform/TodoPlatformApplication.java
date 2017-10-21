5953daefff1c67b14c6b811f818a7cdc570eeaec
Alex Pask <alex.pask@ladbrokescoral.com>
2017-10-21 12:29:54 +0100

    Initial Commit

---
+++ src/main/java/org/alex/todoplatform/TodoPlatformApplication.java
@@ -0,0 +1,12 @@
+package org.alex.todoplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoPlatformApplication.class, args);
+	}
+}
